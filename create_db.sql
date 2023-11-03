4b6164c4f0860dad22ddc77c70aa2f9e83458af3
solea <soleafoly@gmail.com>
2023-11-03 14:18:49 +0200

    script to create a databse and insert data in db

---
+++ create_db.sql
@@ -0,0 +1,57 @@
+-- Create the library database
+CREATE DATABASE library;
+
+-- Use the library database
+USE library;
+
+-- Create the Employee table
+CREATE TABLE Employee (
+  employee_id INT PRIMARY KEY,
+  name VARCHAR(255),
+  username VARCHAR(50) UNIQUE,
+  password VARCHAR(50)
+);
+
+-- Create the Users table
+CREATE TABLE Users (
+  user_id INT PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(100),
+  phone_number VARCHAR(15),
+  address VARCHAR(255)
+);
+
+-- Create the Books table
+CREATE TABLE Books (
+  book_id INT PRIMARY KEY,
+  author VARCHAR(255),
+  name VARCHAR(255),
+  genre VARCHAR(50),
+  isLoaned BOOLEAN,
+  publication_date DATE
+);
+
+-- Create the Movies table
+CREATE TABLE Movies (
+  movie_id INT PRIMARY KEY,
+  name VARCHAR(255),
+  movie_studio VARCHAR(100),
+  genre VARCHAR(50),
+  isLoaned BOOLEAN,
+  release_date DATE
+);
+
+-- Create the Loans table for both book and movie loans
+CREATE TABLE Loans (
+  loan_id INT PRIMARY KEY,
+  start_date DATE,
+  end_date DATE,
+  isReturned BOOLEAN,
+  book_id INT,
+  movie_id INT,
+  user_id INT,
+  FOREIGN KEY (book_id) REFERENCES Books(book_id),
+  FOREIGN KEY (movie_id) REFERENCES Movies(movie_id),
+  FOREIGN KEY (user_id) REFERENCES Users(user_id)
+  FOREIGN KEY (employee_id) REFERENCES Employee(employee_id);
+);
---
+++ insert_employees.sql
@@ -0,0 +1,23 @@
+-- Insert 20 instances into the Employee table
+INSERT INTO Employee (employee_id, name, username, password)
+VALUES
+  (1, 'John Doe', 'johndoe1', 'password123'),
+  (2, 'Jane Smith', 'janesmith2', 'secret456'),
+  (3, 'Robert Johnson', 'robertj3', 'password789'),
+  (4, 'Emily White', 'emilyw4', 'secret321'),
+  (5, 'Michael Brown', 'michaelb5', 'password456'),
+  (6, 'Susan Davis', 'susand6', 'secret234'),
+  (7, 'David Wilson', 'davidw7', 'password789'),
+  (8, 'Linda Anderson', 'lindaa8', 'secret567'),
+  (9, 'James Moore', 'jamesm9', 'password123'),
+  (10, 'Karen Lee', 'karenl10', 'secret987'),
+  (11, 'William Garcia', 'williamg11', 'password654'),
+  (12, 'Betty Harris', 'bettyh12', 'secret987'),
+  (13, 'Richard Martin', 'richardm13', 'password123'),
+  (14, 'Patricia Scott', 'patricias14', 'secret654'),
+  (15, 'Charles Thomas', 'charlesth15', 'password987'),
+  (16, 'Carol Wilson', 'carolw16', 'secret123'),
+  (17, 'Thomas Allen', 'thomasa17', 'password654'),
+  (18, 'Nancy Clark', 'nancyc18', 'secret789'),
+  (19, 'Daniel Hall', 'danielh19', 'password234'),
+  (20, 'Sandra Turner', 'sandrat20', 'secret321');
